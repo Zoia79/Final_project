f4f353eff2937239b070407ab689032d132ce27b
Zoia79 <tumorxonova@gmail.com>
2025-01-23 23:12:20 +0800

    Проверка 1

--- API_tests/tests_api.py
+++ API_tests/tests_api.py
@@ -1,17 +1,1 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
 
-# Настройка драйвера
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# Пробуем открыть страницу
-try:
-    driver.get("https://www.chitai-gorod.ru/")
-    sleep(5)  # Даем браузеру время открыться
-    print("Страница открыта успешно!")
-except Exception as e:
-    print(f"Ошибка открытия страницы: {e}")
-finally:
-    driver.quit()
